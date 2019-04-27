4fc82b68e89e2eece15feac69a7c1ba1035fd5a5
alex <piskun.aleksey@gmail.com>
2019-04-27 15:10:13 +0200

    v0.1.0 processing

---
+++ src/db_commuter/connections.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+
+"""
+Connection to database
+"""
+
+import abc
+
+
+__all__ = [
+    "SqliteConnection",
+    "PgConnection"
+]
+
+
+class Connection(abc.ABC):
+    """
+    Abstract class, provides definitions of basic connection parameters
+    """
+    def __init__(self):
+        self.conn = None
+
+    def __del__(self):
+        """
+        close connection when class object is garbage collected
+        """
+        self.close_connection()
+
+    def get_conn(self):
+        return self.conn
+
+    @abc.abstractmethod
+    def set_connection(self, **kwargs):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def close_connection(self):
+        raise NotImplementedError()
+
+
+class SqliteConnection(Connection):
+    """
+    Establish connection with Sqlite database
+    """
+    def __init__(self, path2db):
+        """
+        :param path2db: path to database file
+        """
+        super().__init__()
+        self.path2db = path2db
+
+    def set_connection(self, **kwargs):
+        raise NotImplementedError()
+
+    def close_connection(self):
+        raise NotImplementedError
+
+
+class PgConnection(Connection):
+    """
+    Establish connection with PostgreSQL database
+    """
+    def __init__(self, host, port, user, password, db_name, ssl_mode):
+        """
+        :param db_name: name of database
+        :param ssl_mode: boolean
+        """
+        super().__init__()
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.db_name = db_name
+        self.ssl_mode = ssl_mode
+
+    def set_connection(self, **kwargs):
+        raise NotImplementedError()
+
+    def close_connection(self):
+        raise NotImplementedError
