3ff2489fc24a80ded15fe9647041cabf32b787e4
alex <piskun.aleksey@gmail.com>
2019-04-27 12:30:14 +0200

    added manager

---
+++ app/config.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from os import path
+
+root_dir = path.dirname(path.abspath(path.join(__file__, '..')))
+
+# path to directory with .sql-scripts
+path2scripts = path.join(root_dir, 'scripts')
+
+# path to secret.env
+path2env = path.join(root_dir, 'app', 'secret.env')
